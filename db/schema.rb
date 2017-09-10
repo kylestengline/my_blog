dec40838c8094f38df8a933017d7a708cea0cb79
Kyle Stengline <kyle.stengline@gmail.com>
2017-09-09 23:03:12 -0700

    timestamps, seeds, updates specs and life in general html

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170909191254) do
+ActiveRecord::Schema.define(version: 20170910053157) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.integer "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
--- spec/features/showing_all_posts_spec.rb
+++ spec/features/showing_all_posts_spec.rb
@@ -7,14 +7,14 @@ RSpec.feature "Showing all posts" do
   end
 
   let(:users) { User.create(email: "user@example.com", password: "password") }
-  let(:posts) { Post.create(title: "All the things", content: "The content of my post", user_id: users.id) }
+  let(:posts) { Post.create(created_at: "9/9/2017", title: "All the things", content: "The content of my post", user_id: users.id) }
 
   scenario "#life-in-general" do
     pending("awaiting login feature")
     
     click_link "Life In General"
     expect(page).to have_current_path life_in_general_path
-    expect(page).to have_content posts.timestamps
+    expect(page).to have_content posts.created_at.to_date
     expect(page).to have_link posts.title
     expect(page).to have_content posts.content
   end
