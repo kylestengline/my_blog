3b737354645367dcfc1b2f349c1d5ad8df996c29
Kyle Stengline <kyle.stengline@gmail.com>
2017-09-14 00:34:16 -0700

    updates posts index to truncate, updates specs, updates and styles welcome page

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,15 +1,17 @@
 "use strict";
 
 $(document).ready(function(){
-  var postLinks = $(".to-posts");
+  var postLinks = $(".posts");
   postLinks.hide();
 
   var firstDiv = $('.in-the-middle');
   var secondDiv = $('.of-all-of-it');
   var firstSet = firstDiv.children();
   var secondSet = secondDiv.children();
+  var footer = $('footer');
   firstSet.hide();
   secondSet.hide();
+  footer.hide();
 
   $("span").each(function(index){
     $(this).delay(150*index).fadeIn(1500);
@@ -17,5 +19,11 @@ $(document).ready(function(){
 
   setTimeout(function(){
     postLinks.fadeIn(1000);
+    footer.fadeIn(1000);
   }, 4500);
+
+  //Hide flash after 2 secs
+  setTimeout(function(){
+    $('#flash_notice').hide();
+  }, 2000);
 });
---
+++ spec/features/logout_user_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.feature "Users Logout" do
+  
+  let(:user) { User.create(email: "me@example.com", password: "password") }
+
+  scenario "#successful logout" do
+
+    sign_in_user user
+
+    click_on "Sign Out"
+
+    expect(page).to have_current_path sign_in_path
+  end
+end
